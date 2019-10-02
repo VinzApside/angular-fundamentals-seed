a1bbb8960294ac2b1017c25d342a516114ce15ca
Vinz <vincentballut@outlook.fr>
2019-10-02 14:46:42 +0200

    create passenger dashboard module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { PassengerDashBoardModule } from "./passenger-dashboard/passenger-dashboard.module";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CommonModule, FormsModule],
+  imports: [
+    //angular module
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    //custom module
+    PassengerDashBoardModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
