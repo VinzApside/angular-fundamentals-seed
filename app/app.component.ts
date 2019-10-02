2661ffa283a7c0f5ff84d5d1e139066821547cd2
Vinz <vincentballut@outlook.fr>
2019-10-02 11:58:31 +0200

    two way databinding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,30 @@ import { Component } from "@angular/core";
   styleUrls: ["app.Component.scss"],
   template: `
     <div class="app">
-      {{ title }}
+      <h1 [innerHTML]="title"></h1>
+      <div>
+        <input
+          type="text"
+          [ngModel]="name"
+          (ngModelChange)="handleChange($event)"
+        />
+      </div>
+      <div>{{ name }}</div>
+      <button (click)="handleClick($event)">Change name</button>
     </div>
   `
 })
 export class AppComponent {
   title: string;
+  name: string = "Todd";
+
+  handleChange(value: string) {
+    this.name = value;
+  }
+  handleClick(event: any) {
+    this.name = "Name reset";
+  }
   constructor() {
-    this.title = "Ultimate Angular";
+    this.title = "Say my name !";
   }
 }
